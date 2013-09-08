92f8ab2ea1ffe723bce952c3111745b5d32cf4a5
Fitra Aditya <fitra@g.pl>
2013-09-08 21:39:28 +0700

    Add WYSIWYG in admin area

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,4 +1,4 @@
-from django import forms
+from django.conf import settings
 from django.contrib import admin
 from django.db import models
 from django.forms import TextInput, Textarea
@@ -15,6 +15,13 @@ class PostAdmin(admin.ModelAdmin):
   list_filter = ['author', 'category', 'status']
   search_fields = ('title', 'body')
   prepopulated_fields = {'slug': ('title',)}
+  formfield_overrides = {
+    models.CharField: {'widget': TextInput(attrs={'size':'140'})},
+    models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':100})},
+  }
+
+  class Media:
+    js = [ settings.ASSETS_URL+'js/tinymce/tiny_mce.js', settings.ASSETS_URL+'js/run_tinymce.js' ]
 
   def save_model(self, request, obj, form, change):
     if not change:
